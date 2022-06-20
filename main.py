f691cb90f58778913cf31f0c2b080e68ffd0c48d
Marko Henien <mhenien@andrew.cmu.edu>
2022-06-20 17:43:15 -0400

    dependencies succesfully installed

--- main.py
+++ main.py
@@ -1,72 +1,144 @@
-# Copyright (c) 2015,2018,2019 MetPy Developers.
-# Distributed under the terms of the BSD 3-Clause License.
-# SPDX-License-Identifier: BSD-3-Clause
 """
-===================
-NEXRAD Level 2 File
-===================
+======================================
+Reading NEXRAD Data from the AWS Cloud
+======================================
 
-Use MetPy to read information from a NEXRAD Level 2 (volume) file and plot
+Within this example, we show how you can remotely access Next Generation Weather Radar (NEXRAD) Data from Amazon Web Services
+and plot quick looks of the datasets.
+
 """
+print(__doc__)
+
+# Author: Max Grover (mgrover@anl.gov)
+# License: BSD 3 clause
+
+import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
-import numpy as np
 
-from metpy.cbook import get_test_data
-from metpy.io import Level2File
-from metpy.plots import add_metpy_logo, add_timestamp
+import pyart
 
-###########################################
+######################################%%
+# Read NEXRAD Level 2 Data
+# ------------------------
+#
+# Let's start first with NEXRAD Level 2 data, which is ground-based radar data collected
+# by the National Oceanic and Atmospheric Administration (NOAA), as a part of the National Weather Service
+# ### Configure our Filepath for NEXRAD Level 2 Data
+# We will access data from the **noaa-nexrad-level2** bucket, with the data organized as:
+# 
+#    ``s3://noaa-nexrad-level2/year/month/date/radarsite/{radarsite}{year}{month}{date}_{hour}{minute}{second}_V06``
+# 
+# Where in our case, we are using a sample data file from Houston, Texas (KHGX)
+# on March 22, 2022, at 1201:25 UTC. This means our path would look like:
 
-# Open the file
-name = get_test_data('KTLX20200520_201643_V06.gz', as_file_obj=False)
-f = Level2File(name)
+aws_nexrad_level2_file = "s3://noaa-nexrad-level2/2022/03/22/KHGX/KHGX20220322_120125_V06"
 
-print(f.sweeps[0][0])
+#######################################
+# We can use the **pyart.io.read_nexrad_archive** module to access our data, passing in the filepath.
 
-###########################################
+radar = pyart.io.read_nexrad_archive(aws_nexrad_level2_file)
 
-# Pull data out of the file
-sweep = 0
+#######################################
+# Let's take a look at a summary of what fields are available.
 
-# First item in ray is header, which has azimuth angle
-az = np.array([ray[0].az_angle for ray in f.sweeps[sweep]])
-diff = np.diff(az)
-diff[diff > 180] -= 360.
-diff[diff < -180] += 360.
-avg_spacing = diff.mean()
-az = (az[:-1] + az[1:]) / 2
-az = np.concatenate(([az[0] - avg_spacing], az, [az[-1] + avg_spacing]))
+list(radar.fields)
 
-# 5th item is a dict mapping a var name (byte string) to a tuple
-# of (header, data array)
-ref_hdr = f.sweeps[sweep][0][4][b'REF'][0]
-ref_range = (np.arange(ref_hdr.num_gates + 1) - 0.5) * ref_hdr.gate_width + ref_hdr.first_gate
-ref = np.array([ray[4][b'REF'][1] for ray in f.sweeps[sweep]])
+#######################################
+# Let's plot the reflectivity/velocity fields as a first step to investigating our dataset.
+# 
+# Note: the reflectivity and velocity fields are in different sweeps, so we will need to specify which sweep to plot in each plot.
 
-rho_hdr = f.sweeps[sweep][0][4][b'RHO'][0]
-rho_range = (np.arange(rho_hdr.num_gates + 1) - 0.5) * rho_hdr.gate_width + rho_hdr.first_gate
-rho = np.array([ray[4][b'RHO'][1] for ray in f.sweeps[sweep]])
+fig = plt.figure(figsize=(12,4))
+display = pyart.graph.RadarMapDisplay(radar)
 
-###########################################
-fig, axes = plt.subplots(1, 2, figsize=(15, 8))
-add_metpy_logo(fig, 190, 85, size='large')
-for var_data, var_range, ax in zip((ref, rho), (ref_range, rho_range), axes):
-    # Turn into an array, then mask
-    data = np.ma.array(var_data)
-    data[np.isnan(data)] = np.ma.masked
+ax = plt.subplot(121,
+                 projection=ccrs.PlateCarree())
 
-    # Convert az,range to x,y
-    xlocs = var_range * np.sin(np.deg2rad(az[:, np.newaxis]))
-    ylocs = var_range * np.cos(np.deg2rad(az[:, np.newaxis]))
+display.plot_ppi_map('reflectivity',
+                     sweep=0,
+                     ax=ax,
+                     colorbar_label='Equivalent Relectivity ($Z_{e}$) \n (dBZ)',
+                     vmin=-20,
+                     vmax=60)
 
-    # Plot the data
-    ax.pcolormesh(xlocs, ylocs, data, cmap='viridis')
-    ax.set_aspect('equal', 'datalim')
-    ax.set_xlim(-40, 20)
-    ax.set_ylim(-30, 30)
-    add_timestamp(ax, f.dt, y=0.02, high_contrast=True)
+ax = plt.subplot(122,
+                 projection=ccrs.PlateCarree())
 
+display.plot_ppi_map('velocity',
+                     sweep=1,
+                     ax=ax,
+                     colorbar_label='Radial Velocity ($V_{r}$) \n (m/s)',
+                     vmin=-70,
+                     vmax=70)
+
+#######################################
+# Within this plot, we see that the velocity data still has regions that are folded, indicating the dataset has not yet been dealiased.
+
+#######################################%%
+# Read NEXRAD Level 3 Data
+# ------------------------
+# We can also access NEXRAD Level 3 data using Py-ART!
+# 
+# These datasets have had additional data quality processes applied, including dealiasing.
+# 
+# Each Level 3 data field is stored in **separate file** - in this example, we will look at the reflectivity and 
+# velocity field at the lowest levels. These correspond to the following variable names:
+#
+# - ``N0U`` - Velocity at the lowest level
+# - ``NOQ`` - Reflectivity at the lowest level
+# 
+# These datasets are also in a different bucket (**unidata-nexrad-level3**), and the files are in a **flat directory structure**
+# using the following naming convention:
+#
+#    ``s3://unidata-nexrad-level3/{radarsite}_{field}_{year}_{month}_{date}_{hour}_{minute}_{second}``
+# 
+# For example, we can look at data from that same time as the NEXRAD Level 2 data used previously (March 22, 2022 at 1201 UTC)
+
+aws_nexrad_level3_velocity_file = "s3://unidata-nexrad-level3/HGX_N0U_2022_03_22_12_01_25"
+aws_nexrad_level3_reflectivity_file = "s3://unidata-nexrad-level3/HGX_N0Q_2022_03_22_12_01_25"
+
+#######################################
+#Read our Data using **pyart.io.read_nexrad_level3**
+
+radar_level3_velocity = pyart.io.read_nexrad_level3(aws_nexrad_level3_velocity_file)
+radar_level3_reflectivity = pyart.io.read_nexrad_level3(aws_nexrad_level3_reflectivity_file)
+
+#######################################
+# Let's confirm that each radar object has a single field:
+
+print('velocity radar object: ', 
+      list(radar_level3_velocity.fields),
+      'reflectivity radar object: ',
+      list(radar_level3_reflectivity.fields)
+     )
+
+#######################################
+# Plot a Quick Look of our NEXRAD Level 3 Data
+# 
+# Let's plot the reflectivity/velocity fields as a first step to investigating our dataset.
+# 
+# Note: the reflectivity and velocity fields are in different radars, so we need to setup different displays.
+
+fig = plt.figure(figsize=(12,4))
+reflectivity_display = pyart.graph.RadarMapDisplay(radar_level3_reflectivity)
+
+ax = plt.subplot(121,
+                 projection=ccrs.PlateCarree())
+
+reflectivity_display.plot_ppi_map('reflectivity',
+                                  ax=ax,
+                                  colorbar_label='Equivalent Relectivity ($Z_{e}$) \n (dBZ)',
+                                  vmin=-20,
+                                  vmax=60)
+
+velocity_display = pyart.graph.RadarMapDisplay(radar_level3_velocity)
+
+ax = plt.subplot(122,
+                 projection=ccrs.PlateCarree())
+
+velocity_display.plot_ppi_map('velocity',
+                              ax=ax,
+                              colorbar_label='Radial Velocity ($V_{r}$) \n (m/s)',
+                              vmin=-70,
+                              vmax=70)
 plt.show()
-
-
-
